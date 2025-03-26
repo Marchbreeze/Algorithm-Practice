a5e6fd97a7f735c660a343534b880b06805e5342
Sangho Kim <marchbreeze03@gmail.com>
2025-03-27 00:18:25 +0900

    [Silver 2] 2630. 색종이 만들기

---
+++ baekjoon/_s2_2630_color_paper.py
@@ -0,0 +1,34 @@
+'''
+<2630. 색종이 만들기>
+실버2
+https://www.acmicpc.net/problem/2630
+'''
+
+# 분할정복 & 재귀함수 활용
+
+import sys
+
+N = int(sys.stdin.readline())
+paper = [list(map(int, sys.stdin.readline().split())) for _ in range(N)] 
+
+result = []
+
+def solution(x, y, N) :
+  color = paper[x][y]
+  for i in range(x, x+N) :
+    for j in range(y, y+N) :
+      if color != paper[i][j] :
+        solution(x, y, N//2)
+        solution(x, y+N//2, N//2)
+        solution(x+N//2, y, N//2)
+        solution(x+N//2, y+N//2, N//2)
+        return
+  if color == 0 :
+    result.append(0)
+  else :
+    result.append(1)
+
+
+solution(0,0,N)
+print(result.count(0))
+print(result.count(1))
